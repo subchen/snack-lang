311d240070717965c1ac7302cd097c06e05eb9d3
Guoqiang Chen <subchen@gmail.com>
2015-05-29 09:38:43 +0000

    add array.flatten()

---
+++ test/test-array-flatten.js
@@ -0,0 +1,14 @@
+var assert = require('chai').assert;
+var flatten = require('../lib/array/flatten');
+
+/* jshint mocha: true */
+describe('flatten', function() {
+
+    it('Flattens a nested array', function() {
+        assert.deepEqual(flatten([1, [2, [3]]]), [1, 2, [3]]);
+    });
+
+    it('Flattens a nested array (deep)', function() {
+        assert.deepEqual(flatten([1, [2, [3]]], true), [1, 2, 3]);
+    });
+});
